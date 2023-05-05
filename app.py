9d2b1cca88fa44eb7c4a74fcbfbefebe41b7c205
Patryk Wisniewicz <wisniewicz.patryk@gmail.com>
2023-05-05 12:37:31 +0200

    feat(): save translate to db via api

--- app.py
+++ app.py
@@ -6,12 +6,19 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 XSALE_ORGANIZATION = connector_db.XSALE_ORGANIZATION
-RAPORT_PATH = 'raport.xlsx'
-LANGUAGE_ID = 9     # MarketplacePL
 
-def get_description_and_title(id, language_id):
+
+RAPORT_PATH = 'raport.xlsx'
+
+LANGUAGE_ID = 9     # MarketplacePL
+TARGET_LANGUAGE_ID = 2 # Angielski en-US
+
+SOURCE_LANG = 'PL'
+TARGET_LANG = 'EN'
+
+
+def get_description_and_title(id):
     url = f" https://api.xsale.ai/{XSALE_ORGANIZATION}/articles/{id}/translations"
     payload = {}
     headers = {
@@ -23,7 +30,7 @@ def get_description_and_title(id, language_id):
     data = json.loads(response.text)
 
     for i in range(len(data)):
-        if data[i]['LanguageId'] == language_id:
+        if data[i]['LanguageId'] == LANGUAGE_ID:
             return {
                 "title": data[i]['Name'],
                 "description": data[i]['Description']
@@ -38,14 +45,14 @@ def save_description_and_title_from_api():
     df["description"] = ""
 
     for index, row in df.iterrows():
-        df.loc[df['xSale_ID'] == df.loc[index]['xSale_ID'], "title"] = get_description_and_title(df.loc[index]['xSale_ID'], LANGUAGE_ID)['title'] 
-        df.loc[df['xSale_ID'] == df.loc[index]['xSale_ID'], "description"] = get_description_and_title(df.loc[index]['xSale_ID'], LANGUAGE_ID)['description']
+        df.loc[df['xSale_ID'] == df.loc[index]['xSale_ID'], "title"] = get_description_and_title(df.loc[index]['xSale_ID'])['title'] 
+        df.loc[df['xSale_ID'] == df.loc[index]['xSale_ID'], "description"] = get_description_and_title(df.loc[index]['xSale_ID'])['description']
 
     df.to_excel(f"{RAPORT_PATH}", index=False)
 
-def translate_deepl(text, source_lang, target_lang):    
+def translate_deepl(text):    
     url = "https://api-free.deepl.com/v2/translate"
-    payload = {'text': text.encode('utf-8'), 'target_lang': target_lang, 'tag_handling': 'html', 'ignore_tags': 'img', 'source_lang': source_lang}
+    payload = {'text': text.encode('utf-8'), 'target_lang': TARGET_LANG, 'tag_handling': 'html', 'ignore_tags': 'img', 'source_lang': SOURCE_LANG}
     headers = {
     'Authorization': f'DeepL-Auth-Key {os.environ.get("DEEPL_KEY")}',
     'Content-Type': 'application/x-www-form-urlencoded'
@@ -56,22 +63,60 @@ def translate_deepl(text, source_lang, target_lang):
 
     return data["translations"][0]["text"]
 
-def translate_from_xlsx():
-    
+def translate_from_xlsx():    
     df = pd.read_excel(RAPORT_PATH)
 
-    source_lang = 'PL'
-    target_lang = 'EN'
-
-    df[f"title_{target_lang}"] = ""
-    df[f"description_{target_lang}"] = ""
+    df[f"title_{TARGET_LANG}"] = ""
+    df[f"description_{TARGET_LANG}"] = ""
 
     for index, row in df.iterrows():
-        df.loc[df['title'] == df.loc[index]['title'], f"title_{target_lang}"] = translate_deepl(df.loc[index]['title'], source_lang, target_lang)
-        df.loc[df['description'] == df.loc[index]['description'], f"description_{target_lang}"] = translate_deepl(df.loc[index]['description'], source_lang, target_lang)
+        df.loc[df['title'] == df.loc[index]['title'], f"title_{TARGET_LANG}"] = translate_deepl(df.loc[index]['title'])
+        df.loc[df['description'] == df.loc[index]['description'], f"description_{TARGET_LANG}"] = translate_deepl(df.loc[index]['description'])
 
     df.to_excel(f"{RAPORT_PATH}", index=False)
     print("done")
 
+def update_translation_in_xsale(id, name, description):
+    url = f"https://api.xsale.ai/{XSALE_ORGANIZATION}/articles/{id}/translations"
+
+    payload = {
+    "LanguageId": TARGET_LANGUAGE_ID,
+    "Name": name,
+    "Description": description
+    }
+
+    # Encode payload to UTF-8
+    encoded_payload = {}
+    for k, v in payload.items():
+        if isinstance(v, str):
+            encoded_payload[k] = v.encode('utf-8').decode('utf-8')
+        else:
+            encoded_payload[k] = v
+
+    # Convert dictionary to JSON string
+    payload_str = json.dumps(encoded_payload)
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + connector_db.authorization,
+        'x-id-token': connector_db.x_id_token
+    }
+
+    response = requests.put(url, headers=headers, data=payload_str)
+    # response = requests.put(url, headers=headers, data=payload)
+    return response.status_code
+
+def update_translation_from_xlsx():
+    df = pd.read_excel(RAPORT_PATH)
+    df["Response_code"] = ""
+
+    for index, row in df.iterrows():        
+        df.loc[df['xSale_ID'] == df.loc[index]['xSale_ID'], "Response_code"] = update_translation_in_xsale(df.loc[index]['xSale_ID'], df.loc[index][f'title_{TARGET_LANG}'], df.loc[index][f'description_{TARGET_LANG}']) 
+
+    df.to_excel(f"{RAPORT_PATH}", index=False)
+    print("koniec")
+
+update_translation_from_xlsx()
 
 
